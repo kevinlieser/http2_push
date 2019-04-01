5c62866f6887518d041118d1fc08eae5f626ce07
Kevin Lieser <info@ka-mediendesign.de>
2019-04-01 09:33:32 +0200

    Added TYPO3 9 LTS support

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,5 +4,5 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('http2_push').'/Classes/Hooks/ContentPostProcessor.php:Ka\\Http2Push\\Hooks\\ContentPostProcessor->renderAll';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('http2_push').'/Classes/Hooks/ContentPostProcessor.php:Ka\\Http2Push\\Hooks\\ContentPostProcessor->renderOutput';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = 'Ka\\Http2Push\\Hooks\\ContentPostProcessor->renderAll';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = 'Ka\\Http2Push\\Hooks\\ContentPostProcessor->renderOutput';
